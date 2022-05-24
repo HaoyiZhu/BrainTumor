43c03b1ab3a5e2219b74bf49d9c1f329e4095911
Jessica-legend <legend0018@sjtu.edu.cn>
2022-05-24 17:47:48 +0000

    run the SNN

--- brain_tumor/models/vit_2d.py
+++ brain_tumor/models/vit_2d.py
@@ -51,7 +51,7 @@ class Attention(nn.Module):
         project_out = not (heads == 1 and dim_head == dim)
 
         self.heads = heads
-        self.scale = dim_head**-0.5
+        self.scale = dim_head ** -0.5
 
         self.attend = nn.Softmax(dim=-1)
         self.dropout = nn.Dropout(dropout)
